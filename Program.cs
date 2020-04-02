a5bdf68b31ae87827f5d0427e19919328dff6d93
RicardoBaniski <ricardo.baniski@opet.edu.br>
2020-04-02 19:00:38 -0300

    Implementação dos métodos de tratamento dos dados recebidos

--- Program.cs
+++ Program.cs
@@ -24,7 +24,7 @@ namespace Imobiliaria
                 {
                     item.Categoria = "APARTAMENTO";
                 }
-                if (item.Categoria.Contains("CASA"))
+                if (item.Categoria.Contains("CAS"))
                 {
                     item.Categoria = "CASA";
                 }
@@ -48,6 +48,7 @@ namespace Imobiliaria
                 item.Status = item.Status.Replace(" E ", ",");
 
                 // AREA TOTAL
+                item.Area_total = item.Area_total.Replace(".", ",");
                 if (!item.Area_total.Contains(","))
                 {
                     item.Area_total = item.Area_total + ",00";
@@ -61,6 +62,7 @@ namespace Imobiliaria
                 }
 
                 // CONDOMINIO
+                item.Condominio = item.Condominio.Replace(".", ",");
                 if (item.Condominio == null || item.Condominio == "0")
                 {
                     item.Condominio = "0,00";
@@ -116,8 +118,185 @@ namespace Imobiliaria
                     item.Elevador = "NAO INFORMADO";
                 }
 
+                // CHURRASQUEIRA
+                item.Churrasqueira = item.Churrasqueira.ToUpper();
+                if (item.Churrasqueira.Contains("SIM"))
+                {
+                    item.Churrasqueira = "PRIVATIVA";
+                }
+                if (item.Churrasqueira.Contains("NÃO"))
+                {
+                    item.Churrasqueira = "NAO";
+                }
+
+                // DORMITORIOS
+                item.Dormitorios = item.Dormitorios.ToUpper();
+                if (item.Dormitorios == "TRÊS")
+                {
+                    item.Dormitorios = "3";
+                }
+                switch (item.Dormitorios)
+                {
+                    case null:
+                        item.Dormitorios = "0";
+                        break;
+                    case "UM":
+                        item.Dormitorios = "1";
+                        break;
+                    case "DOIS":
+                        item.Dormitorios = "2";
+                        break;
+                    case "TRES":
+                        item.Dormitorios = "3";
+                        break;
+                    case "QUATRO":
+                        item.Dormitorios = "4";
+                        break;
+                    case "CINCO":
+                        item.Dormitorios = "5";
+                        break;
+                    case "SEIS":
+                        item.Dormitorios = "6";
+                        break;
+                    case "SETE":
+                        item.Dormitorios = "7";
+                        break;
+                    case "OITO":
+                        item.Dormitorios = "8";
+                        break;
+                    case "NOVE":
+                        item.Dormitorios = "9";
+                        break;
+                }
+
+                // SUITES
+                item.Suites = item.Suites.ToUpper();
+                if (item.Suites == "TRÊS")
+                {
+                    item.Suites = "3";
+                }
+                switch (item.Suites)
+                {
+                    case null:
+                        item.Suites = "0";
+                        break;
+                    case "UM":
+                        item.Suites = "1";
+                        break;
+                    case "DOIS":
+                        item.Suites = "2";
+                        break;
+                    case "TRES":
+                        item.Suites = "3";
+                        break;
+                    case "QUATRO":
+                        item.Suites = "4";
+                        break;
+                    case "CINCO":
+                        item.Suites = "5";
+                        break;
+                    case "SEIS":
+                        item.Suites = "6";
+                        break;
+                    case "SETE":
+                        item.Suites = "7";
+                        break;
+                    case "OITO":
+                        item.Suites = "8";
+                        break;
+                    case "NOVE":
+                        item.Suites = "9";
+                        break;
+                }
+
+                // VAGAS
+                item.Vagas = item.Vagas.ToUpper();
+                if (item.Vagas == "TRÊS")
+                {
+                    item.Vagas = "3";
+                }
+                switch (item.Vagas)
+                {
+                    case null:
+                        item.Vagas = "0";
+                        break;
+                    case "UM":
+                        item.Vagas = "1";
+                        break;
+                    case "DOIS":
+                        item.Vagas = "2";
+                        break;
+                    case "TRES":
+                        item.Vagas = "3";
+                        break;
+                    case "QUATRO":
+                        item.Vagas = "4";
+                        break;
+                    case "CINCO":
+                        item.Vagas = "5";
+                        break;
+                    case "SEIS":
+                        item.Vagas = "6";
+                        break;
+                    case "SETE":
+                        item.Vagas = "7";
+                        break;
+                    case "OITO":
+                        item.Vagas = "8";
+                        break;
+                    case "NOVE":
+                        item.Vagas = "9";
+                        break;
+                }
+
+                // BANHEIROS
+                item.Banheiros = item.Banheiros.ToUpper();
+                if (item.Banheiros == "TRÊS")
+                {
+                    item.Banheiros = "3";
+                }
+                switch (item.Banheiros)
+                {
+                    case null:
+                        item.Banheiros = "0";
+                        break;
+                    case "UM":
+                        item.Banheiros = "1";
+                        break;
+                    case "DOIS":
+                        item.Banheiros = "2";
+                        break;
+                    case "TRES":
+                        item.Banheiros = "3";
+                        break;
+                    case "QUATRO":
+                        item.Banheiros = "4";
+                        break;
+                    case "CINCO":
+                        item.Banheiros = "5";
+                        break;
+                    case "SEIS":
+                        item.Banheiros = "6";
+                        break;
+                    case "SETE":
+                        item.Banheiros = "7";
+                        break;
+                    case "OITO":
+                        item.Banheiros = "8";
+                        break;
+                    case "NOVE":
+                        item.Banheiros = "9";
+                        break;
+                }
+
                 // UNIDADE FEDERATIVA
                 item.Uf = item.Uf.ToUpper();
+
+                // LATITUDE
+                item.Latitude = item.Latitude.ToString().Replace(".", ",");
+
+                // longitude
+                item.Longitude = item.Longitude.ToString().Replace(".", ",");
 
                 // VALOR DE VENDA
                 if (item.Valor_venda == null)
@@ -138,15 +317,16 @@ namespace Imobiliaria
                     item.Valor_aluguel = item.Valor_aluguel.ToString().Replace("R$", "");
                     item.Valor_aluguel = item.Valor_aluguel.ToString().Replace(".", ",");
                 }
-                Console.WriteLine(item.Elevador);
             }
-            Console.ReadKey();
-
             string path = @"C:\TEMP\JsonTratado.json";
             XmlSerializer xmlSerializer = new XmlSerializer(imoveis.GetType());
             xmlSerializer.Serialize(Console.Out, imoveis);
 
             File.WriteAllText(path, JsonConvert.SerializeObject(imoveis));
+
+            Console.Clear();
+            Console.WriteLine("TRATAMENTO CONCLUÍDO: " + path);
+            Console.ReadKey();
         }
     }
 }
